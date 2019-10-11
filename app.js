99f8f30197d490dfdaa3a2b1ee35591decc593a5
Paul Moreland <office@Pauls-iMac-Pro.home>
2019-10-12 01:21:01 +0200

    kmom06v2

--- app.js
+++ app.js
@@ -52,7 +52,14 @@ io.on('connection', function (socket) {
 
     socket.on('chat message', function (message) {
         count ++;
-        let d = new Date().toUTCString();
+        let now = new Date();
+        let yr = now.getFullYear();
+        let mth = ((now.getMonth() + 1) < 10 ? "0" + now.getMonth() + 1 : now.getMonth() + 1);
+        let day = (now.getDate() >= 10 ? now.getDate() : "0" + now.getDate());
+        let hr = (now.getHours() >= 10 ? now.getHours() : "0" + now.getHours());
+        let min = (now.getMinutes() >= 10 ? now.getMinutes() : "0" + now.getMinutes());
+        let sec = (now.getSeconds() >= 10 ? now.getSeconds() : "0" + now.getSeconds());
+        let d = `${day}/${mth}/${yr}, ${hr}:${min}:${sec}`;
 
         conversation.push({
             count: count,
